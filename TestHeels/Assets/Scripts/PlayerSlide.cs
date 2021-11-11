a537c9df06cbcc2eeb0d03ea46733f22e37c66ae
Illya Chernov <12scream12@gmail.com>
2021-11-11 14:27:52 +0200

    slide trigger done

--- TestHeels/Assets/Scripts/PlayerSlide.cs
+++ TestHeels/Assets/Scripts/PlayerSlide.cs
@@ -23,15 +23,38 @@ public class PlayerSlide : MonoBehaviour
     {
         if (!isRotated) playerYPosition = transform.position.y;
 
-        if (Input.GetKeyDown(KeyCode.R))
+        //if (Input.GetKeyDown(KeyCode.R))
+        //{
+        //    isRotated = true;
+
+        //    float colliderCenter = heelCollider.size.y * 0.5f;
+        //    RotateHeelsCollider(Quaternion.Euler(0f, 0f, 90f), new Vector3(-colliderCenter, -0.25f, 0f));
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.T))
+        //{
+        //    isRotated = false;
+
+        //    transform.position = new Vector3(transform.position.x, playerYPosition, transform.position.z);
+
+        //    RotateHeelsCollider(originalRotation, originalPosition);
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("SlideTrigger"))
         {
             isRotated = true;
 
             float colliderCenter = heelCollider.size.y * 0.5f;
             RotateHeelsCollider(Quaternion.Euler(0f, 0f, 90f), new Vector3(-colliderCenter, -0.25f, 0f));
         }
+    }
 
-        if (Input.GetKeyDown(KeyCode.T))
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("SlideTrigger"))
         {
             isRotated = false;
 

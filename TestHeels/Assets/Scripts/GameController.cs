4772f691b42ce182f493a8c13c363db430350f72
Illya Chernov <12scream12@gmail.com>
2021-11-14 18:09:34 +0200

    level indicator added

--- TestHeels/Assets/Scripts/GameController.cs
+++ TestHeels/Assets/Scripts/GameController.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public PlayerController playerController;
     public InputController inputController;
+    public Text levelIndexText;
 
     private void Start()
     {
+        float levelIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        levelIndexText.text = "Level " + levelIndex.ToString();
+
         PlayerController.Instance.OnFinishEvent += FinishGame;
         inputController.OnFirstInputEvent += StartGame;
     }
